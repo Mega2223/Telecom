3396669e4b0dda0f57743beb37c382a18519b55a
Mega2223 <mega2223gamer@gmail.com>
2026-03-01 00:14:21 -0300

    mas ue

--- Install/install.lua
+++ Install/install.lua
@@ -51,32 +51,4 @@ for url, loc in string.gmatch(PATHS, "([^\n]+)%-%->([^\n]+)\n") do
     shell.run(string.format("wget \"%s\" \"%s\"",url,loc))
 end
 
-print("Library installed successfully :)")
-
---- E literalmente so copiar o bglh da pasta startup lol
-
-[[ print("Do you wish to setup a [router] module or a [endpoint]?")
-local mode = read()
-
-if mode == "router" then
-    print("what name should this router have? (leave empty for generating name)")
-    ---@type string | nil
-    local name = read()
-    if name == "" then name = nil end
-
-    print("which router firmware module should be implemented?")
-    require('Network.RouterLogic.Firmware.RouterFirmware')
-    for key, value in pairs(ROUTER_FIRMWARE_WRAPPERS) do
-        print(string.format("[%d]: %s",key,value.firmware_type_name))
-    end
-    local firmware_id = tonumber(read())
-    if firmware_id == nil or ROUTER_FIRMWARE_WRAPPERS[firmware_id] == nil then print("invalid option") return end
-    local firmware = ROUTER_FIRMWARE_WRAPPERS[firmware_id]
-
-    --TODO o que eu faço a partir daqui lmao
-
-    print("setting up startup module for router")
-    shell.run("cp ./Startup")
-    os.reboot()
-end
-]]
+print("Library installed successfully :), check the Startup folder for startup profiles")
