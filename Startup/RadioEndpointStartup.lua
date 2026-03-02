1bda8f934687cf1334f61fd8d074c8811d570d5c
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-03-01 22:14:39 -0300

    when the points end

--- Startup/RadioEndpointStartup.lua
+++ Startup/RadioEndpointStartup.lua
@@ -1,6 +1,9 @@
 require('Telecom')
 require('Network.EndpointLogic.Endpoint')
+require('Network.EndpointLogic.Firmware.RadioEndpoint')
 
 FREQUENCY = 2223
 
-endpoint_r = Endpoint()
+endpoint_r = RadioEndpoint(FREQUENCY)
+
+endpoint_r:begin()
