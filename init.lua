bd1558b4ff928b4453096dbe19b80ef618a41fea
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-03-03 22:37:32 -0300

    debuga mais

--- init.lua
+++ init.lua
@@ -5,11 +5,15 @@ package.path = package.path .. ";Telecom/?;Telecom/?.lua"
 ---@type OutputStream<string>
 STD_OUT = print
 ---@type OutputStream<string>
-STD_ERR = function (data)
-    if os and term then
+STD_ERR = function(data)
+    if os and term and fs then
         local col = term.getTextColor()
         term.setTextColor(colors.red)
         print(data)
         term.setTextColor(col)
+        local f = fs.open("error.txt", "w")
+        if not f then return end
+        f.write(data)
+        f.close()
     end
 end
