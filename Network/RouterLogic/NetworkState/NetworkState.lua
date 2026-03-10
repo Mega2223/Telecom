a9da46a4ca0914222ee8689288962e7c3ed263c5
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-03-09 22:48:51 -0300

    fix router not forgetting endpoints in it's own networkstate instance

--- Network/RouterLogic/NetworkState/NetworkState.lua
+++ Network/RouterLogic/NetworkState/NetworkState.lua
@@ -73,6 +73,7 @@ function NetworkState(router_object)
                     self.network_routers[name] = nil
                 end
             end
+            self_in_network.connected_endpoints = {}
             for index, endpoint in pairs(self.router.memory.connected_endpoints) do
                 self_in_network.connected_endpoints[endpoint.address] =
                     NetworkEndpoint(self_in_network,self.router.current_time_milis,endpoint.address)
